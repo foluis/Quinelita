009ea13baf7e9c87a4f23a4e01983f7a059b82b4
Luis Fernando Forero <lforero@globaltelesourcing.com>
2018-09-20 18:56:02 -0500

    Aqui vamos (antes del refactor de ResultadoQuinela)

---
+++ Quinelita.Api/Controllers/QuinelasJornadaController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Quinelita.Data;
+
+namespace Quinelita.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuinelasJornadaController : ControllerBase
+    {
+		private readonly QuinelitaContext _context;
+
+		public QuinelasJornadaController(QuinelitaContext context)
+		{
+			_context = context;
+		}
+
+
+		[Route("Jornada/{jornadaId}")]
+		[HttpGet]
+		public IEnumerable<QuinelaJornada> Get(int jornadaId)
+		{
+			var result = _context.QuinelasJornada
+				//.Include(p => p.Partido)
+				.Where(l => l.Partido.JornadaId == jornadaId);
+
+			return result;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Post([FromBody] QuinelaJornada quinelaJornada)
+		{
+			_context.QuinelasJornada.Add(quinelaJornada);
+			await _context.SaveChangesAsync();
+
+			return Ok();
+		}
+	}
+}
---
+++ Quinelita.Api/Controllers/ResultadosQuinelaController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Quinelita.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quinelita.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ResultadosQuinelaController : ControllerBase
+	{
+		private readonly QuinelitaContext _context;
+
+		public ResultadosQuinelaController(QuinelitaContext context)
+		{
+			_context = context;
+		}
+
+		[Route("Jornada/{jornadaId}")]
+		[HttpGet]
+		public IEnumerable<ResultadoQuinela> Get(int jornadaId)
+		{
+			return _context.ResultadosQuinela				
+				.Where(l => l.Partido.JornadaId == jornadaId);
+		}
+
+		[Route("Jornada/{jornadaId}")]
+		[HttpGet]
+		public async Task<IActionResult> Get(int jornadaId)
+		{
+			List<ResultadoQuinela> resultadosQuinela = new List<ResultadoQuinela>();
+
+			var quinelasJornada = _context.QuinelasJornada
+				.Where(l => l.Partido.JornadaId == jornadaId);
+
+			var resultadosJornada = _context.ResultadosJornada
+				.Where(l => l.Partido.JornadaId == jornadaId);
+			
+
+			foreach (var resultado in resultadosJornada)
+			{
+				var grupoPartidoN = quinelasJornada.Where(p => p.PartidoId == resultado.PartidoId);
+
+				foreach (var partido in grupoPartidoN)
+				{
+					if(resultado.GanadorId == partido.GanadorId)
+					{
+
+					}
+				}
+			}
+
+			return Ok();
+		}
+	}
+}
