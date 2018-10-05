0b45c0fa8707a6903cb8693236a481ffbc7756b7
Luis Fernando Forero <lforero@globaltelesourcing.com>
2018-10-05 17:00:12 -0500

    Add jornadas controller to angular project

---
+++ Quinelita.Web/Controllers/JornadasController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Quinelita.Data;
+
+namespace Quinelita.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JornadasController : ControllerBase
+    {
+		private readonly QuinelitaContext _context;
+
+		public JornadasController(QuinelitaContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public IEnumerable<Jornada> Get()
+		{
+			var result = _context.Jornadas.Select(c =>
+				new Jornada
+				{
+					Id = c.Id,
+					Fecha = c.Fecha,
+					Partidos = c.Partidos
+				});
+
+			return result;
+		}
+
+		[Route("{jornadaId}")]
+		[HttpGet]
+		public IEnumerable<Jornada> Get(int jornadaId)
+		{
+			var result = _context.Jornadas.Select(c =>
+				new Jornada
+				{
+					Id = c.Id,
+					Fecha = c.Fecha,
+					Partidos = c.Partidos
+				}).Where(x => x.Id == jornadaId);
+
+			return result;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Post([FromBody] string fecha)
+		{
+			Jornada jornada = new Jornada()
+			{
+				Fecha = DateTime.Parse(fecha)
+			};
+
+			_context.Jornadas.Add(jornada);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction("Get", new { id = jornada.Id }, jornada);
+		}
+
+		[HttpPut("{id}")]
+		public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Jornada jornada)
+		{
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			if (id != jornada.Id)
+			{
+				return BadRequest();
+			}
+
+			_context.Entry(jornada).State = EntityState.Modified;
+
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!JornadaExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return NoContent();
+		}
+
+		private bool JornadaExists(int id)
+		{
+			return _context.Jornadas.Any(e => e.Id == id);
+		}
+	}
+}
--- Quinelita.Web/Startup.cs
+++ Quinelita.Web/Startup.cs
@@ -1,70 +1,74 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Quinelita.Data;
 
 namespace Quinelita.Web
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<QuinelitaContext>(options => options.UseSqlServer(
+				Configuration.GetConnectionString("QuinelitaDatabase")));
 
-            // In production, the Angular files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/dist";
-            });
-        }
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
+			// In production, the Angular files will be served from this directory
+			services.AddSpaStaticFiles(configuration =>
+			{
+				configuration.RootPath = "ClientApp/dist";
+			});
+		}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseSpaStaticFiles();
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				app.UseHsts();
+			}
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller}/{action=Index}/{id?}");
-            });
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+			app.UseSpaStaticFiles();
 
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller}/{action=Index}/{id?}");
+			});
 
-                spa.Options.SourcePath = "ClientApp";
+			app.UseSpa(spa =>
+			{
+				// To learn more about options for serving an Angular SPA from ASP.NET Core,
+				// see https://go.microsoft.com/fwlink/?linkid=864501
 
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
-            });
-        }
-    }
+				spa.Options.SourcePath = "ClientApp";
+
+				if (env.IsDevelopment())
+				{
+					spa.UseAngularCliServer(npmScript: "start");
+				}
+			});
+		}
+	}
 }
