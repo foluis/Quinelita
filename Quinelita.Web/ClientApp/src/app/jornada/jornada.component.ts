9fda959dbf921b5aa9f67570bc954dbf0c05ab0e
Luis Fernando Forero <lforero@globaltelesourcing.com>
2018-10-09 17:28:18 -0500

    creating services

--- Quinelita.Web/ClientApp/src/app/jornada/jornada.component.ts
+++ Quinelita.Web/ClientApp/src/app/jornada/jornada.component.ts
@@ -1,14 +1,34 @@
-﻿import { Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     selector: 'jornada',
     templateUrl: './jornada.component.html',
     styleUrls: ['./jornada.component.css']
 })
-/** jornada component*/
-export class JornadaComponent {
-    /** jornada ctor */
-    constructor() {
 
-    }
+export class JornadaComponent {
+  jornadas: any[];
+  private url = 'http://localhost:56292/api/jornadas';
+
+    constructor(private http:HttpClient) {
+
+      http.get(this.url)
+        .subscribe(response => {
+          this.jornadas = response as any[];
+          //console.log(response);
+        }, error => console.error(error));
+  }
+
+  createJornada(nombreJornada: HTMLInputElement) {
+    var d = new Date();
+    var date = d.getDate()
+    let jornada = {
+      fecha: d
+    };
+    this.http.post(this.url, JSON.stringify(jornada))
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
 }
