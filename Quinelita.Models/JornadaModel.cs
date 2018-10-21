90ae06a69cc0738f976f6e7e08ada131058e8e52
Luis Fernando <foluis@hotmail.com>
2018-10-21 18:32:27 -0500

    Jornada component

---
+++ Quinelita.Models/JornadaModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quinelita.Models
+{
+    public class JornadaModel
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public bool AbiertaAlPublico { get; set; }
+    }
+}
--- Quinelita.Web/ClientApp/src/app/jornada/jornada.component.ts
+++ Quinelita.Web/ClientApp/src/app/jornada/jornada.component.ts
@@ -1,34 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-    selector: 'jornada',
-    templateUrl: './jornada.component.html',
-    styleUrls: ['./jornada.component.css']
+  selector: 'jornada',
+  templateUrl: './jornada.component.html',
+  styleUrls: ['./jornada.component.css']
 })
 
 export class JornadaComponent {
   jornadas: any[];
-  private url = 'http://localhost:56292/api/jornadas';
+  private url = 'https://localhost:44322/api/jornadas';
+  private baseUrl: string;
 
-    constructor(private http:HttpClient) {
+  constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
 
-      http.get(this.url)
-        .subscribe(response => {
-          this.jornadas = response as any[];
-          //console.log(response);
-        }, error => console.error(error));
+    this.baseUrl = baseUrl + 'api/jornadas';
+
+    http.get(this.baseUrl)
+      .subscribe(response => {
+        this.jornadas = response as any[];
+        //console.log(response);
+      }, error => console.error(error));
   }
 
-  createJornada(nombreJornada: HTMLInputElement) {
+  createJornada(nombreJornada: HTMLInputElement) {   
+
     var d = new Date();
     var date = d.getDate()
     let jornada = {
-      fecha: d
+      //fecha: d.toTimeString()
+      fecha: this.formatDate_YYYYMMDD(d)
     };
-    this.http.post(this.url, JSON.stringify(jornada))
+
+    //let stringDate: string = "2018-10-09";
+    
+    //this.http.post(this.url, JSON.stringify(stringDate))
+    //  .subscribe(response => {
+    //    console.log(response);
+    //  });
+
+    this.http.post(this.url, "2018-10-09")
       .subscribe(response => {
         console.log(response);
       });
+  }
+
+  formatDate_YYYYMMDD(date) {
+    var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
   }
 }
