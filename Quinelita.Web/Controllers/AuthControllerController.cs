02427b79f7f48a556d50e7a04091ec8466e99661
Luis Fernando <foluis@hotmail.com>
2018-10-21 19:12:01 -0500

    Add example - return json format

---
+++ Quinelita.Web/Controllers/AuthControllerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Quinelita.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("token")]
+        public IActionResult Token()
+        {
+            var userClaims = new[] { new Claim(ClaimTypes.Name, "Luis Forero") };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("youKey"));
+            var credential = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = new JwtSecurityToken(
+                audience: "localhost:23456",
+                issuer: "otro",
+                expires: DateTime.Now.AddMinutes(3),
+                claims: userClaims,
+                signingCredentials: credential
+                );
+
+            return Ok(token);
+        }
+    }
+}
