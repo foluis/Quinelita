106fcf141203022ed9c1a25defcf895ed04e564d
Luis Fernando <foluis@hotmail.com>
2018-10-21 22:39:43 -0500

    Add JWT Authentication

--- Quinelita.Web/Controllers/AuthControllerController.cs
+++ Quinelita.Web/Controllers/AuthControllerController.cs
@@ -20,7 +20,7 @@ namespace Quinelita.Web.Controllers
         {
             var userClaims = new[] { new Claim(ClaimTypes.Name, "Luis Forero") };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("youKey"));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Your very big key"));
             var credential = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
 
             var token = new JwtSecurityToken(
@@ -31,7 +31,70 @@ namespace Quinelita.Web.Controllers
                 signingCredentials: credential
                 );
 
-            return Ok(token);
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return Ok(tokenString);
+        }
+
+        [HttpPost("token2")]
+        public IActionResult Token2()
+        {
+
+            var header = Request.Headers["Authorization"];
+            if (header.ToString().StartsWith("Basic"))
+            {
+                var credValue = header.ToString().Substring("basic".Length).Trim();
+                var userCredential = Encoding.UTF8.GetString(Convert.FromBase64String(credValue));
+                var userNameNpass = userCredential.Split(':');
+
+                //verify from database
+                var authorize = ValidateUserPassword(userNameNpass[0], userNameNpass[1]);
+
+                if (authorize)
+                {
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("YourSecurityKeyFromApp.config.js"));
+                    var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+                    var userClaims = new[]
+                    {
+                        new Claim(
+                            ClaimTypes.Name, "Luis Forero"
+                        ),
+                        new Claim(
+                            ClaimTypes.MobilePhone, "12345678"
+                        ),
+                        new Claim(
+                            ClaimTypes.Role, "1"
+                        )
+                        ,
+                        new Claim(
+                            ClaimTypes.UserData,"mas cosas, se puede json?"
+                        )
+                    };
+
+                    var token = new JwtSecurityToken(
+                        audience: "localhost:44322",
+                        issuer: "localhost:44322",
+                        expires: DateTime.Now.AddMinutes(9),
+                        claims: userClaims,
+                        signingCredentials: signingCredentials
+                        );
+
+                    var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+
+                    return Ok(tokenString);
+                }
+            }
+
+            return BadRequest("Usuario o clave invalidos");
+        }
+
+        private bool ValidateUserPassword(string user, string pws)
+        {
+            if (user == "luis" && pws == "clave")
+                return true;
+
+            return false;
         }
     }
 }
--- Quinelita.Web/Startup.cs
+++ Quinelita.Web/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -5,70 +6,88 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Quinelita.Data;
+using System.Text;
 
 namespace Quinelita.Web
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddDbContext<QuinelitaContext>(options => options.UseSqlServer(
-				Configuration.GetConnectionString("QuinelitaDatabase")));
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(Options => Options.TokenValidationParameters =
+            new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = "localhost:44322",
+                ValidAudience = "localhost:44322",
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("YourSecurityKeyFromApp.config.js"))
+             });
 
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-			// In production, the Angular files will be served from this directory
-			services.AddSpaStaticFiles(configuration =>
-			{
-				configuration.RootPath = "ClientApp/dist";
-			});
-		}
+            services.AddDbContext<QuinelitaContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("QuinelitaDatabase")));
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseExceptionHandler("/Error");
-				app.UseHsts();
-			}
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
-			app.UseSpaStaticFiles();
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
+        }
 
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller}/{action=Index}/{id?}");
-			});
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
 
-			app.UseSpa(spa =>
-			{
-				// To learn more about options for serving an Angular SPA from ASP.NET Core,
-				// see https://go.microsoft.com/fwlink/?linkid=864501
+            app.UseAuthentication();
+            app.UseCors("All");
 
-				spa.Options.SourcePath = "ClientApp";
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
 
-				if (env.IsDevelopment())
-				{
-					spa.UseAngularCliServer(npmScript: "start");
-				}
-			});
-		}
-	}
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
+        }
+    }
 }
